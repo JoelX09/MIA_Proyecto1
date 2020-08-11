11c3647eb018dff8b708b455a567b2b7038d6bf3
Joel <3002421820101@ingenieria.usac.edu.gt>
2020-08-10 22:21:01 -0600

    Mkdisk completo

--- mkdisk.go
+++ mkdisk.go
@@ -1,6 +1,13 @@
 package main
 
-import "os"
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func crearDisco(size int, path string, name string, unit byte) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
@@ -10,10 +17,52 @@ func crearDisco(size int, path string, name string, unit byte) {
 		}
 	}
 
-	f, err := os.Create(path + name)
-	if err != nil {
-		panic(err)
+	temp := strings.Split(name, ".")
+	if temp[1] == "dsk" {
+		if size > 0 {
+			f, err := os.Create(path + name)
+			defer f.Close()
+			if err != nil {
+				panic(err)
+			}
+			var cero int8 = 0
+			var binario bytes.Buffer
+
+			f.Seek(0, 0)
+			binary.Write(&binario, binary.BigEndian, cero)
+			writeNextBytes(f, binario.Bytes())
+
+			if unit == 'k' || unit == 'K' {
+				tam := 1024 * size
+				f.Seek(int64(tam-1), 0)
+				binary.Write(&binario, binary.BigEndian, cero)
+				writeNextBytes(f, binario.Bytes())
+
+			} else if unit == 0 || (unit == 'm' || unit == 'M') {
+				tam := 1024 * 1024 * size
+				f.Seek(int64(tam-1), 0)
+				binary.Write(&binario, binary.BigEndian, cero)
+				writeNextBytes(f, binario.Bytes())
+
+			} else {
+				fmt.Println("Error de unidad de tamano de disco")
+			}
+
+		} else {
+			fmt.Println("El tamano definido para el disco debe ser mayor a cero")
+		}
+	} else {
+		fmt.Println("No se puede crear el disco, Extension invalida.")
 	}
-	f.Close()
+
+}
+
+func writeNextBytes(file *os.File, bytes []byte) {
+
+	_, err := file.Write(bytes)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
