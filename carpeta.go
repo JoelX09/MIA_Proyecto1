46327a3c26d92a6c98ba3fc1308caca8d6ed46b4
Joel <3002421820101@ingenieria.usac.edu.gt>
2020-09-07 23:35:32 -0600

    MKDIR corregido ap indiriecto, pendiente ugo

--- carpeta.go
+++ carpeta.go
@@ -28,9 +28,6 @@ func crearCarpeta(vd string, path string, p bool) {
 
 			if path == "/" {
 				fmt.Println("Se va a crear la carpeta Raiz")
-				/*listadobitmap(rutaDisco, superBloque.SBapBAVD, int(superBloque.SBsizeStructAVD))
-				fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-				fmt.Scanln()*/
 
 				nuevoAVD := avd{}
 				for j := 0; j < 6; j++ {
@@ -42,42 +39,25 @@ func crearCarpeta(vd string, path string, p bool) {
 				copy(nuevoAVD.AVDfechaCreacion[:], fecha)
 				copy(nuevoAVD.AVDnombreDirectorio[:], path)
 				nuevoAVD.AVDproper = 1
-				/*fmt.Println("Avd raiz")
-				fmt.Println(nuevoAVD)
-				fmt.Println("En la posicion")
-				fmt.Println(superBloque.SBapAVD)
-				fmt.Println("EN la ruta")
-				fmt.Println(rutaDisco)
-				fmt.Println("Posicion bitmap")
-				fmt.Println(superBloque.SBfirstFreeBitAVD)
-				fmt.Println("posicion en el espacio para avd")
-				fmt.Println(superBloque.SBapBAVD)*/
 
 				escribirStructAVD(rutaDisco, superBloque.SBapAVD, nuevoAVD)
 				superBloque.SBavdFree--
 				actualizarValorBitmap(rutaDisco, superBloque.SBapBAVD, '1')
 				nuevoFFBAVD := obtenerFirstFreeBit(rutaDisco, superBloque.SBapBAVD, int(tamPart))
-				/*fmt.Println(nuevoFFBAVD)
-				fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-				fmt.Scanln()*/
+
 				superBloque.SBfirstFreeBitAVD = nuevoFFBAVD
 				escribirSuperBloque(rutaDisco, inicioPart, superBloque)
-				/*listadobitmap(rutaDisco, superBloque.SBapBAVD, int(superBloque.SBsizeStructAVD))
-				fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-				fmt.Scanln()*/
+
 			} else {
 				fmt.Println("Crear carpetas en raiz")
 				path1 := strings.TrimPrefix(path, "/")
 				path2 := strings.TrimSuffix(path1, "/")
 				pathPart := strings.Split(path2, "/")
 
-				//i := 0
 				encontrado := false
 				posEncontrado := superBloque.SBapAVD
 				t := 0
-				/*for i := 0; i < len(pathPart); i++ {
-					fmt.Println(pathPart[i])
-				}*/
+
 				for i := 0; i < len(pathPart); i++ {
 					t = i
 					fmt.Println(pathPart[i])
@@ -93,17 +73,11 @@ func crearCarpeta(vd string, path string, p bool) {
 					} else {
 						if p == true {
 							for i := 0; i < len(pathPart); i++ {
-								/*fmt.Println("Entro")
-								fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-								fmt.Scanln()*/
 								superBloque = obtenerSB(rutaDisco, inicioPart)
 								crearDir(rutaDisco, superBloque, path2, inicioPart)
-								/*fmt.Println("Salgo")
-								fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-								fmt.Scanln()*/
 							}
 						} else {
-							fmt.Println("No se pueden crear las carpetas padres, falta de parametro")
+							fmt.Println("No se pueden crear las carpetas padres, falta de parametro de permiso")
 						}
 					}
 				}
@@ -124,12 +98,12 @@ func crearDir(rutaDisco string, superBloque sb, path2 string, inicioPart int64) 
 	pathPart := strings.Split(path2, "/")
 	for i := 0; i < len(pathPart); i++ {
 		posAnterior = posEncontrado
-		/*fmt.Println("BUsco en la pos")
-		fmt.Println(posEncontrado)
-		fmt.Println("EL directotiro")
-		fmt.Println(pathPart[i])*/
 		encontrado, posEncontrado = buscarDir(posEncontrado, pathPart[i], rutaDisco)
 		if encontrado == false {
+			posEncontrado = nuevoAVDindirecto(posAnterior, rutaDisco, superBloque, inicioPart)
+			posAnterior = posEncontrado
+			superBloque = obtenerSB(rutaDisco, inicioPart)
+
 			nuevoAVD := avd{}
 			for j := 0; j < 6; j++ {
 				nuevoAVD.AVDapArraySub[j] = -1
@@ -141,23 +115,9 @@ func crearDir(rutaDisco string, superBloque sb, path2 string, inicioPart int64) 
 			copy(nuevoAVD.AVDnombreDirectorio[:], pathPart[i])
 			nuevoAVD.AVDproper = 1
 
-			/*fmt.Println("Nuevo avd")
-			fmt.Println(nuevoAVD)
-			fmt.Println("creo directorio")
-			fmt.Println(pathPart[i])*/
-
-			posFirstFreeBit := superBloque.SBfirstFreeBitAVD //obtenerFirstFreeBit(rutaDisco, superBloque.SBapBAVD, int(superBloque.SBsizeStructAVD))
-			//listadobitmap(rutaDisco, superBloque.SBapBAVD, int(superBloque.SBsizeStructAVD))
-			/*fmt.Println("POsicion en bitmap avd libre")
-			fmt.Println(posFirstFreeBit)
-			fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-			fmt.Scanln()*/
+			posFirstFreeBit := superBloque.SBfirstFreeBitAVD
 
 			posNuevoAVD := superBloque.SBapAVD + (posFirstFreeBit * superBloque.SBsizeStructAVD)
-			/*fmt.Println("Posicion en la que insertare avd")
-			fmt.Println(posNuevoAVD)
-			fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-			fmt.Scanln()*/
 
 			avdPadre := obtenerAVD(rutaDisco, posAnterior)
 
@@ -167,67 +127,91 @@ func crearDir(rutaDisco string, superBloque sb, path2 string, inicioPart int64) 
 					break
 				}
 			}
-			/*fmt.Println("Actualizo datos del padre, esta en la pos")
-			fmt.Println(posAnterior)
-			fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-			fmt.Scanln()*/
+
 			escribirStructAVD(rutaDisco, posAnterior, avdPadre)
-			/*fmt.Println("Ingreso el nuevo avd en la pos")
-			fmt.Println(posNuevoAVD)
-			fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-			fmt.Scanln()*/
+
 			escribirStructAVD(rutaDisco, posNuevoAVD, nuevoAVD)
 			superBloque.SBavdFree--
-			/*fmt.Println("****************************************************************")
-			fmt.Println("Valor 1 al bitmap avd en la pos")
-			fmt.Println(superBloque.SBapBAVD + posFirstFreeBit)
-			fmt.Println("**********************************************************************")*/
 			actualizarValorBitmap(rutaDisco, superBloque.SBapBAVD+posFirstFreeBit, '1')
 			posFirstFreeBit = obtenerFirstFreeBit(rutaDisco, superBloque.SBapBAVD, int(superBloque.SBsizeStructAVD))
-			/*fmt.Println("*************************************************************")
-			fmt.Println("Nuevo primer bit libre")
-			fmt.Println(posFirstFreeBit)
-			fmt.Println("*************************************************************")*/
 			superBloque.SBfirstFreeBitAVD = posFirstFreeBit
 
 			escribirSuperBloque(rutaDisco, inicioPart, superBloque)
 
 			fmt.Println("Se creo el directorio: " + pathPart[i])
-			/*fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-			fmt.Scanln()*/
 			break
 		}
 	}
-	/*fmt.Println("Saliendo ptm....")
-	fmt.Println("\nEjecuacion pausada... Presione enter para continuar")
-	fmt.Scanln()
-	fmt.Println("CSTM")*/
+
+}
+
+func nuevoAVDindirecto(pos int64, ruta string, superBloque sb, inicioPart int64) int64 {
+
+	arbol := obtenerAVD(ruta, pos)
+	var posEncontrado int64 = 0
+	cantidad := 0
+	for i := 0; i < len(arbol.AVDapArraySub); i++ {
+		if arbol.AVDapArraySub[i] != -1 {
+			cantidad++
+		} else if arbol.AVDapArraySub[i] == -1 {
+			posEncontrado = pos
+			break
+		}
+	}
+
+	if cantidad == 6 {
+		if arbol.AVDapAVD != -1 {
+			posEncontrado = nuevoAVDindirecto(arbol.AVDapAVD, ruta, superBloque, inicioPart)
+		} else {
+			nuevoAVD := avd{}
+			for j := 0; j < 6; j++ {
+				nuevoAVD.AVDapArraySub[j] = -1
+			}
+			nuevoAVD.AVDapDetalleDir = -1
+			nuevoAVD.AVDapAVD = -1
+			fecha := time.Now().Format("2006-01-02 15:04:05")
+			copy(nuevoAVD.AVDfechaCreacion[:], fecha)
+			nuevoAVD.AVDnombreDirectorio = arbol.AVDnombreDirectorio
+			nuevoAVD.AVDproper = 1
+
+			posFirstFreeBit := superBloque.SBfirstFreeBitAVD
+
+			posNuevoAVD := superBloque.SBapAVD + (posFirstFreeBit * superBloque.SBsizeStructAVD)
+
+			arbol.AVDapAVD = posNuevoAVD
+
+			escribirStructAVD(ruta, pos, arbol)
+
+			escribirStructAVD(ruta, posNuevoAVD, nuevoAVD)
+			superBloque.SBavdFree--
+			actualizarValorBitmap(ruta, superBloque.SBapBAVD+posFirstFreeBit, '1')
+			posFirstFreeBit = obtenerFirstFreeBit(ruta, superBloque.SBapBAVD, int(superBloque.SBsizeStructAVD))
+			superBloque.SBfirstFreeBitAVD = posFirstFreeBit
+
+			escribirSuperBloque(ruta, inicioPart, superBloque)
+
+			tempNombre := ""
+
+			for i := 0; i < len(arbol.AVDnombreDirectorio); i++ {
+				if arbol.AVDnombreDirectorio[i] != 0 {
+					tempNombre += string(arbol.AVDnombreDirectorio[i])
+				}
+			}
+
+			posEncontrado = posNuevoAVD
+		}
+	}
+	return posEncontrado
 }
 
 func buscarDir(pos int64, nombre string, ruta string) (bool, int64) {
 
 	arbol := obtenerAVD(ruta, pos)
 	encontrado := false
-	/*fmt.Println("Buscar en el avd:")
-	fmt.Println(arbol)
-	fmt.Println("Presione enter para continuar")
-	fmt.Scanln()
-	fmt.Println("El nombre")
-	fmt.Println(nombre)
-	fmt.Println("Presione enter para continuar")
-	fmt.Scanln()*/
 	var posEncontrado int64 = 0
 	for i := 0; i < len(arbol.AVDapArraySub); i++ {
-		/*fmt.Println("Analizando pos")
-		fmt.Println(i)
-		fmt.Println("Presione enter para continuar")
-		fmt.Scanln()*/
 
 		if arbol.AVDapArraySub[i] != -1 {
-			/*fmt.Println("Las pos de abajo esta ocupada")
-			fmt.Println(i)
-			fmt.Println("Presione enter para continuar")
-			fmt.Scanln()*/
 			temp := obtenerAVD(ruta, arbol.AVDapArraySub[i])
 			nomb := ""
 			for j := 0; j < len(temp.AVDnombreDirectorio); j++ {
@@ -271,12 +255,6 @@ func obtenerFirstFreeBit(ruta string, posIni int64, tam int) int64 {
 		if err != nil {
 			log.Fatal("binary.Read failed", err)
 		}
-		/*fmt.Println("Posicion bitmap obteniendo First free")
-		fmt.Println(posIni)
-		fmt.Println("Valor en la posicion")
-		fmt.Println(car)
-		fmt.Println("valor i")
-		fmt.Println(i)*/
 		if car != 49 {
 			pos = i
 			break
